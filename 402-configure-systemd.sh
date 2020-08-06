f2640ffb7eb5a7b5acb7117f141f3a76e90b4c34
Martin Kovacs <martin.k.kovacs@gmail.com>
2020-08-06 13:51:47 +0200

    fixed 402

--- 402-configure-systemd.sh
+++ 402-configure-systemd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-$sec=5s
+sec=5s
 
 echo -e "##########\nSetting DefaultTimeoutStopSec to $sec in /etc/systemd/system.conf\n##########"
 sudo sed -i "s/^#*DefaultTimeoutStopSec/DefaultTimeoutStopSec=$sec/g" /etc/systemd/system.conf
