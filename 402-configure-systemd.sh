236b33e35726bb3a60c1744b2bcc23aacd4a9647
Martin Kovacs <martin.k.kovacs@gmail.com>
2020-08-09 13:18:45 +0200

    fixed regex

--- 402-configure-systemd.sh
+++ 402-configure-systemd.sh
@@ -3,7 +3,7 @@
 sec=5s
 
 echo -e "##########\nSetting DefaultTimeoutStopSec to $sec in /etc/systemd/system.conf\n##########"
-sudo sed -i "s/^#*DefaultTimeoutStopSec/DefaultTimeoutStopSec=$sec/g" /etc/systemd/system.conf
+sudo sed -i "s/^#*DefaultTimeoutStopSec=[0-9]*s*/DefaultTimeoutStopSec=$sec/g" /etc/systemd/system.conf
 
 echo -e "##########\nReloading systemd daemon.\n##########"
 sudo systemctl daemon-reload
