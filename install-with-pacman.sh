1a582a818cf7fc0d3a85207ec5ee255f57c83321
Martin Kovacs <martin.k.kovacs@gmail.com>
2020-08-18 15:29:33 +0200

    removed redundant '--needed' flags

--- install-with-pacman.sh
+++ install-with-pacman.sh
@@ -3,7 +3,7 @@
 for pkg in "$@"; do
     if ! pacman -Q $pkg &> /dev/null; then
         echo -e "##########\nInstalling $pkg package.\n##########"
-        sudo pacman -S --noconfirm --needed $pkg
+        sudo pacman -S --noconfirm $pkg
     else
         echo -e "##########\nThe package $pkg is already installed.\n##########"
     fi
--- install-with-yay.sh
+++ install-with-yay.sh
@@ -3,7 +3,7 @@
 for pkg in "$@"; do
     if ! yay -Q $pkg &> /dev/null; then
         echo -e "##########\nInstalling $pkg package.\n##########"
-        yay -S --noconfirm --needed $pkg
+        yay -S --noconfirm $pkg
     else
         echo -e "##########\nThe package $pkg is already installed.\n##########"
     fi
