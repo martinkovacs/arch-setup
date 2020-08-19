d8abbf2a504baa8e0ad90080950a4de70481011b
Martin Kovacs <martin.k.kovacs@gmail.com>
2020-08-19 17:11:14 +0200

    updated 100

--- 100-set-cores-to-makepkg.sh
+++ 100-set-cores-to-makepkg.sh
@@ -4,10 +4,10 @@ numberofcores=$(grep -c ^processor /proc/cpuinfo)
 
 function set_core_count {
     echo -e "##########\nChanging the makeflags for "$numberofcores" cores.\n##########"
-    sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j17"/g' /etc/makepkg.conf
+    sudo sed -i 's/#MAKEFLAGS="-j2"/MAKEFLAGS="-j'$(($numberofcores+1))'"/g' /etc/makepkg.conf;
     
     echo -e "##########\nChanging the compression settings for "$numberofcores" cores.\n##########"
-    sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T '$numberofcores' -z -)/g' /etc/makepkg.conf
+    sudo sed -i 's/COMPRESSXZ=(xz -c -z -)/COMPRESSXZ=(xz -c -T '"$numberofcores"' -z -)/g' /etc/makepkg.conf
 }
 
 set_core_count
